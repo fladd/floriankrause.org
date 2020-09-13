ae39092b960cd5cb91640cbe2fd4876466724921
fladd <florian.krause@fladd.de>
2020-09-14 00:02:29 +0200

    favicon fix

--- _stuffpages/config.py
+++ _stuffpages/config.py
@@ -34,7 +34,7 @@ html_head = [
     '<link href="$style" rel="stylesheet" media="screen">',
     '<link rel="stylesheet" href="https://cdn.rawgit.com/jpswalsh/academicons/master/css/academicons.min.css">',
     '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">',
-    '<link rel="icon" href="{{favicon}}" type="image/x-icon" />'
+    '<link rel="icon" href="$favicon" type="image/x-icon" />'
 ]
 
 # HTML navigation
